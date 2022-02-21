ac39be2ca43330b1d376e849fa6304ebb553d711
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-02-21 14:32:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import imgFavFruit from './assets/manga.jpg' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state ={
+    nome:"Lucas",
+    idade:20,
+    comidaFavorita:"Prato feito",
+    musicasFavoritas:["Minha felicidade","Era só mais um Silva","Experience","Neopolitan Dreams-Remix"]
+  }
+  render(){
+    return(
+      <div>
+      <h1>lista de informações:{this.state.nome}</h1>
+      <h2>Idade: {this.state.idade} </h2>
+      <h3>Comida favorita: {this.state.comidaFavorita} </h3>
+      <h3>Musicas favoritas:</h3>
+      <ul>
+        <li> {this.state.musicasFavoritas[0]} </li>
+        <li> {this.state.musicasFavoritas[1]} </li>
+        <li> {this.state.musicasFavoritas[2]} </li>
+        <li> {this.state.musicasFavoritas[3]} </li>
+      </ul>
+      <h3>Fruta favorita:</h3>
+      <img src={imgFavFruit} />
+
     </div>
-  );
-}
-
-export default App;
+    )
+    }
+}export default App;
